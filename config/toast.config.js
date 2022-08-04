edc464e7d2e9d722c156c90879d1adf1f66f8a17
vlhsmylv <iam.valeh.ismayilov@gmail.com>
2022-08-05 01:11:28 +0400

    init

---
+++ config/toast.config.js
@@ -0,0 +1,9 @@
+const toastConfig = () => {
+   return {
+       pauseOnFocusLoss: false,
+       pauseOnHover: false,
+       autoClose: 2000
+   }
+}
+
+export default toastConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import '../styles/globals.css'
+import "bootstrap/dist/css/bootstrap.css"
+import {ToastContainer} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css"
+            integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A=="
+            crossOrigin="anonymous" referrerPolicy="no-referrer"/>
+      <title>URL Shortener</title>
+    </Head>
+    <Component {...pageProps} />
+    <ToastContainer limit={1} />
+  </>
 }
 
 export default MyApp
